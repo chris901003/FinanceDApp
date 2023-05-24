19c8a7eac1ba6c161b3a4e849fa59d8a56bbe82e
chris901003 <a0987999103@gmail.com>
2023-05-24 23:07:47 +0800

    賺取代幣畫面

--- src/core/main/earnMoneyView.vue
+++ src/core/main/earnMoneyView.vue
@@ -1,5 +1,138 @@
 <template>
-    <div style="font-size: 20rem;">
-        Earn Money View
+    <div id="top-style">
+        <div style="display: flex; margin-right: auto">
+            <img src="../../assets/navigation/earn-money.png" alt="EarnMoney" style="height: 15rem; width: 15rem">
+            <p id="title-info">賺錢</p>
+        </div>
+        <p id="top-info">通過以下問題即可獲得100代幣</p>
+        <div id="question-count-section">
+            <p style="font-size: 12rem;">{{ currentQuestionCount }}</p>
+            <p style="font-size: 25rem; line-height: 25rem;">/</p>
+            <p style="font-size: 12rem; line-height: 30rem;">10</p>
+        </div>
+        <div id="question">
+            <p>{{ firstNumber }}</p>
+            <img src="../../assets/earnMoney/plus.png" alt="+">
+            <p>{{ secondNumber }}</p>
+        </div>
+        <div id="answer">
+            <img src="../../assets/earnMoney/equal.png" alt="=">
+            <input type="text" id="answer-input-box" v-model="answer">
+            <div id="answer-under-line"></div>
+        </div>
+        <button id="submit-button" @click="sumbitAnswer">發送</button>
+        <div id="submit-button-under-line"></div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const firstNumber = ref(0)
+const secondNumber = ref(0)
+const answer = ref("0")
+const currentQuestionCount = ref(1)
+
+function sumbitAnswer() {
+    console.log("比對答案")
+}
+</script>
+
+<style scoped>
+#top-style {
+    margin: 0rem 20rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+#title-info {
+    font-size: 18rem;
+    line-height: 16rem;
+    margin-left: 3rem;
+}
+#top-info {
+    font-size: 15em;
+    margin-top: 30rem;
+}
+#question-count-section {
+    position: absolute;
+    right: 50rem;
+    top: 55rem;
+    display: flex;
+}
+#question {
+    display: flex;
+    flex-direction: row;
+    margin-top: 15rem;
+}
+#question p {
+    font-size: 20rem;
+    margin: 0rem 50rem;
+    line-height: 18rem;
+}
+#question img {
+    height: 15rem;
+    width: 15rem;
+}
+#answer {
+    display: flex;
+    flex-direction: column;
+    margin-top: 20rem;
+}
+#answer img {
+    position: absolute;
+    height: 15rem;
+    width: 15rem;
+    margin-top: 7rem;
+    margin-right: 5rem;
+    transform: translateX(-30rem);
+}
+#answer-input-box {
+    all: unset;
+    font-size: 20rem;
+    text-align: center;
+    line-height: 20rem;
+    width: 100rem;
+}
+#answer-under-line {
+    height: 1rem;
+    width: 100rem;
+    margin-top: -8rem;
+    border-bottom: solid 1rem #9be6fc;
+}
+#submit-button {
+    all: unset;
+    margin-top: 30rem;
+    font-size: 12rem;
+    background-color: #FFD966;
+    padding: 2rem 20rem;
+    border-radius: 3rem;
+    animation: submit-button-animation 0.5s ease-in-out alternate infinite;
+}
+@keyframes submit-button-animation {
+    from {
+        transform: translateY(0px);
+    }
+    to {
+        transform: translateY(5rem);
+        box-shadow: 0px 1rem 10px 3px rgb(0, 0, 0, 0.5);
+    }
+}
+#submit-button:hover {
+    cursor: pointer;
+}
+#submit-button-under-line {
+    transform: translateY(9rem);
+    animation: submit-button-under-line-animation 0.5s ease-in-out alternate infinite;
+}
+@keyframes submit-button-under-line-animation {
+    from {
+        border-bottom: solid 1rem #FFD966;
+        width: 20rem;
+    }
+    to {
+        border-bottom: solid 1rem #FFD966;
+        width: 80rem;
+    }
+}
+</style>
